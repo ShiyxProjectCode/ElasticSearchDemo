03e207e13c25ab49204ffc640f643b604ec05ff8
syxtest01@gmail.com <xing_*666888>
2016-10-24 10:34:51 +0800

    修改接口命名

--- ElasticSearch.WebApi/Controllers/ESTestApiController.cs
+++ ElasticSearch.WebApi/Controllers/ESTestApiController.cs
@@ -7,36 +7,33 @@ using System.Web.Http;
 namespace ElasticSearch.WebApi.Controllers
 {
     [RoutePrefix("api")]
-    public class ESTestApiController : BaseApiController
+    public class EsTestApiController : BaseApiController
     {
-        [Route("estest")]
+        [Route("search")]
         [HttpGet]
         public object Search()
         {
-            //1 搜索数据
             string key = GetStringRequest("Key");
             int? from = GetIntRequest("from");
             int? size = GetIntRequest("size");
 
-            return ElasticSearchHelper.Instance.Search("db_test", "person", key ?? "方鸿渐个人", from == null ? 0 : from.Value, size == null ? 20 : size.Value);
+            return ElasticSearchHelper.Instance.Search("test_index", "person", key ?? "方鸿渐个人", from == null ? 0 : from.Value, size == null ? 20 : size.Value);
         }
 
-        [Route("estest/SearchFullFileds")]
+        [Route("esapi/SearchFullFileds")]
         [HttpGet]
         public object SearchFullFileds()
         {
-            //1 搜索数据
             string key = GetStringRequest("Key");
             int? from = GetIntRequest("from");
             int? size = GetIntRequest("size");
             return ElasticSearchHelper.Instance.SearchFullFileds("test_index", "person", key ?? "方鸿渐个人", @from ?? 0, size ?? 20);
         }
 
-        [Route("estest/SearchFullFiledss")]
+        [Route("esapi/SearchFullFileds1")]
         [HttpGet]
-        public object SearchFullFiledss()
+        public object SearchFullFileds1()
         {
-            //1 搜索数据
             string key = GetStringRequest("Key");
             int? from = GetIntRequest("from");
             int? size = GetIntRequest("size");
@@ -47,9 +44,9 @@ namespace ElasticSearch.WebApi.Controllers
         /// 索引数据
         /// </summary>
         /// <returns></returns>
-        [Route("estest/index")]
+        [Route("esapi/CreateData")]
         [HttpGet]
-        public object index()
+        public object CreateData()
         {
             string index = "test_index";
             string type = "person";
@@ -65,7 +62,7 @@ namespace ElasticSearch.WebApi.Controllers
             Random rdName = new Random();
             ParallelOptions _po = new ParallelOptions();
             _po.MaxDegreeOfParallelism = 4;
-            Parallel.For(0, 10000, _po, c =>
+            Parallel.For(0, 1000000, _po, c =>
             {
                 var start = rd.Next(0, S.test.Length - 700);
                 var startName = rd.Next(0, S.test.Length - 30);
